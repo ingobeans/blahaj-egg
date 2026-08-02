874fc531fde647d53365b512eea111ccc8402faa
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-08-02 04:39:11 +0200

    make window draggable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 slint::slint! {
     export component App inherits Window {
-        background: rgba(0, 0, 0, 0.0);
+        background: rgba(0, 0, 0, 0.5);
         no-frame: true;
         title: "Blåhaj";
         width: 415px;
@@ -9,11 +9,41 @@ slint::slint! {
 
         Image { source: @image-url("backdrop.png");}
         Image { source: @image-url("blahaj.png"); width: 300px; x:100px;}
-        Image { source: @image-url("egg.png");}
+        img:= Image { source: @image-url("egg.png");}
+        callback mouse_move(length, length);
+
+        TouchArea {
+            moved => {
+                if (self.pressed ) {
+                    root.mouse_move(self.mouse_x - self.pressed_x,self.mouse_y - self.pressed_y);
+                }
+            }
+        }
     }
 }
 
 fn main() {
     println!("{}, v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-    App::new().unwrap().run().unwrap()
+    let pin_win = App::new().unwrap();
+
+    pin_win
+        .window()
+        .set_position(slint::LogicalPosition::new(0., 0.));
+
+    let pin_win_clone = pin_win.as_weak();
+    pin_win.on_mouse_move(move |delta_x, delta_y| {
+        let pin_win_clone = pin_win_clone.unwrap();
+        let logical_pos = pin_win_clone
+            .window()
+            .position()
+            .to_logical(pin_win_clone.window().scale_factor());
+        pin_win_clone
+            .window()
+            .set_position(slint::LogicalPosition::new(
+                logical_pos.x + delta_x,
+                logical_pos.y + delta_y,
+            ));
+    });
+
+    pin_win.run().unwrap();
 }
