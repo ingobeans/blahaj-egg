e9f2cbd84c014e1102bb5dd877cb604ddf5123a2
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-08-02 04:20:19 +0200

    add egg..

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 slint::slint! {
     export component App inherits Window {
-        background: rgba(0, 0, 0, 0);
+        background: rgba(0, 0, 0, 0.0);
         no-frame: true;
         title: "Blåhaj";
-        Text { text: "Hello World!"; }
+        width: 415px;
+        height: 559px;
+        Text { text: "Hello World!"; x:0;y:0;}
 
-        Image { source: @image-url("blahaj.png"); }
+        Image { source: @image-url("backdrop.png"); width: 415px;}
+        Image { source: @image-url("blahaj.png"); width: 300px; x:100px;}
+        Image { source: @image-url("egg.png"); width: 415px;}
     }
 }
 
